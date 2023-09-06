c0933aff91f1fefa22c31ebc8c384e0033cb7224
Tanner <127629835+tannersmith40@users.noreply.github.com>
2023-09-05 20:19:37 -0400

    Update Home.vue

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -3,15 +3,41 @@
 </script>
 
 <template>
-    <div class="d-flex  justify-content-center ">
-        <div class="text-center">
+    <div class="d-flex flex-column align-items-center">
+        <div class="text-center mb-5">
             <p class="h1">
                 Welcome to Pupdate! We update your puppy advertisements for you!
             </p>
+        </div>
+        <div class="text-center mt-4">
+            <p class="h5">
+                How it works:
+            </p>
+            <ol class="text-left">
+                <li>After signing up and creating an account, click on the “Login Credentials” tab.</li>
+                <li>Input your Lancaster/Buckeye email, password, and phone number that you use to sign into that account, and click save.</li>
+                <li>When you’re ready, click on the “Payments” tab, and choose the amount of puppies you need updated, and pay via bank or card.</li>
+                <li>That’s it! After payment is received, we will begin updating your online puppy advertisements same day.</li>
+            </ol>
         </div>
     </div>
 </template>
 
 <style scoped>
-
+    .text-center {
+        text-align: center;
+    }
+    .mt-4 {
+        margin-top: 1.5rem;
+    }
+    .mb-5 {
+        margin-bottom: 3rem;
+    }
+    .h5 {
+        font-size: 1.25rem;
+    }
+    ol {
+        max-width: 500px;
+        margin: auto;
+    }
 </style>
